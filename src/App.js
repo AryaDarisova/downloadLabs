cf112caf3b1f78535d9d1d0197a3d918e0a3524a
Дмитрий Шорин <balalajkalodocnik@gmail.com>
2020-03-23 21:49:25 +0400

    Сделана (почти) стартовая страница

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 import Home from './home/index';
+import { Navbar, NavbarBrand, Button, Nav, NavItem, NavLink, Form, FormGroup, Input } from 'reactstrap';
+import newFolder from './newFolder.png';
+import Login from "./home/login";
+import Registration from "./home/registration";
+
 
 function App() {
+  const [isOpenLogin, setIsOpenLogin] = useState(false);
+  const toggleLogin = () => setIsOpenLogin(!isOpenLogin);
+
+  const [isOpenRegistration, setIsOpenRegistration] = useState(false);
+  const toggleRegistration = () => setIsOpenRegistration(!isOpenRegistration);
+
   return (
     <div className="App">
+        <Navbar color="dark" expand="md">
+            <NavbarBrand href="#">
+                <img src={newFolder} />
+            </NavbarBrand>
+            <Nav className="mr-auto" navbar>
+                <NavItem>
+                    <Button color="dark">На главную</Button>
+                </NavItem>
+                <NavItem>
+                    <Button color="dark">Кафедры</Button>
+                </NavItem>
+                <NavItem>
+                    <Button color="dark">Преподаватели</Button>
+                </NavItem>
+                <NavItem>
+                    <Button color="dark">Учебные материалы</Button>
+                </NavItem>
+            </Nav>
+            <Form inline>
+                <FormGroup className="mr-2">
+                    <Input type="text" placeholder="Найти.." />
+                </FormGroup>
+                <FormGroup>
+                    <Button color="success">Найти</Button>
+                </FormGroup>
+            </Form>
+            <Nav className="ml-auto" navbar>
+                <NavItem className="mr-2">
+                    <Button color="dark" onClick={toggleLogin}>Вход</Button>
+                </NavItem>
+                <NavItem>
+                    <Button color="dark" onClick={toggleRegistration}>Регистрация</Button>
+                </NavItem>
+            </Nav>
+        </Navbar>
+        <Login isOpen={isOpenLogin} close={toggleLogin} />
+        <Registration isOpen={isOpenRegistration} close={toggleRegistration} />
       <Home />
     </div>
   );
--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from '../global/navigation';
 import Main from "./main";
 
 const Home = () => {
     return (
         <div className="home">
-            <Navigation />
             <Main />
         </div>
     );
