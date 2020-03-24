959579cb1932836b475201d3acc2d8061e27b1db
DrewBoiii <goodchickenboy@mail.ru>
2020-03-24 16:23:24 +0400

    init

---
+++ src/main/java/com/example/downloadlabs/DownloadLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.downloadlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/downloadlabs/DownloadLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.downloadlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
