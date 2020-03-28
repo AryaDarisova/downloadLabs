f24dfb9e1c30cfeca02910c92b3bf78349f6bb85
DrewBoiii <goodchickenboy@mail.ru>
2020-03-28 17:36:22 +0400

    implementation  * add SecurityConfig  * add UserDetailsServiceImpl  * add authorization

--- src/main/java/com/example/downloadlabs/persistence/dao/UserRepository.java
+++ src/main/java/com/example/downloadlabs/persistence/dao/UserRepository.java
@@ -3,8 +3,10 @@ package com.example.downloadlabs.persistence.dao;
 import com.example.downloadlabs.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+    Optional<User> getUserByEmail(String email);
 
 }
--- src/main/java/com/example/downloadlabs/persistence/model/User.java
+++ src/main/java/com/example/downloadlabs/persistence/model/User.java
@@ -1,8 +1,11 @@
 package com.example.downloadlabs.persistence.model;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Data
@@ -12,7 +15,7 @@ import java.util.Set;
 @Builder
 @Entity
 @Table(name = "person")
-public class User extends AbstractEntity {
+public class User extends AbstractEntity implements UserDetails {
 
     @Column(unique = true, nullable = false)
     private String password;
@@ -24,5 +27,35 @@ public class User extends AbstractEntity {
     @PrimaryKeyJoinColumn
     @Column(name = "role_id")
     private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
